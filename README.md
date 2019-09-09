06f6ff2869140f70d3bea853f5b8e0135297b493
GislaineJessica <GislainehJessica@gmail.com>
2019-09-08 21:43:10 -0300

    Server-Json e Mutation functions

--- README.md
+++ README.md
@@ -1,44 +1,44 @@
- // Para criar a pasta 
+ # Para criar a pasta 
 
  `mkdir graphql-server` 
  
- // Para iniciar um projeto NodeJs
+ # Para iniciar um projeto NodeJs
  
  `npm init`
  
- // Instalar dependências necessárias para o servidor
+ # Instalar dependências necessárias para o servidor
  
  `npm install express express-graphql nodemon`
  
- // Criando script pra rodar servidor que irá atualizar automaticamnete
+ # Criando script pra rodar servidor que irá atualizar automaticamnete
  
  on sripts => `"dev:server" : "nodemon server.js"`
  
- // Rodando servidor
+ # Rodando servidor
  
  `npm run dev:server`
  
- // Nesse ponto não temos nenuma rota criada, então se tentarmos acessar a rota , vai dar erro
+ # Nesse ponto não temos nenuma rota criada, então se tentarmos acessar a rota , vai dar erro
  
  Cannot GET /
  
- // Criando rota unica que iramos acessar para todas as requisições
+ # Criando rota unica que iramos acessar para todas as requisições
  
  `app.use('/graphql', expressGraphql({ schema: schema, graphiql: true,}))` 
  
  -  expressGraphql requer uma variavel de configuração com o `schema` e uma variavel `graphigl = true`, para poder acessar e usar a IDE para fazer os teste.
  
- // Instalando outras dependencias
+ # Instalando outras dependencias
  
  `npm install graphql`
  
- // Criando o schema (root query)
+ # Criando o schema (root query)
 
- // Acessar ide via browser, com a rota principal setada, podemos acessar a ide para fazer os testes no navegador
+ # Acessar ide via browser, com a rota principal setada, podemos acessar a ide para fazer os testes no navegador
  
  `http://localhost:4000/graphql`
  
- // ---------------------- Criar nova branch para mandar pro GitHub ----------------------- //
+ # ---------------------- Criar nova branch para mandar pro GitHub ----------------------- 
  
  `git init`
  
@@ -55,3 +55,14 @@
  `git remote add novaBranch graphql-server http://github.com/gislainejessica/Graphql.git`
  
  `git push --set-upstream graphql-server novaBranch`
+
+ # Instalando novas dependencias
+ `npm install axios json-server`
+ # Add novo script para json-server
+ "json:server" : "json-server --watch data.json"
+ - criando o arquivo data.json que vais servir a aplicação com os dados
+ - Rodar servidor json server
+ `npm run json:server`
+
+ # Mutation (Adicionar, Editar e Deletar)
+ 
--- schema.js
+++ schema.js
@@ -7,49 +7,98 @@ const {
     GraphQLNonNull
 } = require ('graphql');
 
+const axios = require('axios');
+
 // Dados locais para fazer teste de instalação e vê se configuração está funcionando
+
+/*
 const usuarios = [
     {id: 1, nome: "jessica", email: "jessica@email.com", idade: 30},
     {id: 2, nome: "gislaine", email: "gislaine@email.com", idade: 20},
     {id: 3, nome: "tiburcio", email: "tiburcio@email.com", idade: 10}
 ]
+*/
 
 // Tipo Uasuário 
-const UsuarioType = new GraphQLObjectType({
-    name: 'Usuario',
-    fields: () => ({
+const usuarioType = new GraphQLObjectType({
+    name: 'usuario',
+    fields: ({
         id:    {type: GraphQLString},
         nome:  {type: GraphQLString},
         email: {type: GraphQLString},
         idade: {type: GraphQLInt}
     })
 });
+const mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        addUsuarios:{
+            type: usuarioType,
+            args: {
+                nome: {type: new GraphQLNonNull(GraphQLString)},
+                email: {type: new GraphQLNonNull(GraphQLString)},
+                idade: {type: new GraphQLNonNull(GraphQLInt)},
+            },
+            resolve(parentValue, args){
+                return axios.post('http://localhost:3000/usuarios/',{
+                    nome: args.nome,
+                    email:args.email,
+                    idade:args.idade 
+                })
+                .then(res => res.data)
+            }
+        },
+        deleteUsuarios:{
+            type: usuarioType,
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLInt)},
+            },
+            resolve(parentValue, args){
+                return axios.delete(`http://localhost:3000/usuarios/${args.id}`)
+                .then(res => res.data)
+            }
+        },
+        editarUsuarios:{
+            type: usuarioType,
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLInt)},
+                nome: {type: GraphQLString},
+                email: {type: GraphQLString},
+                idade: {type:GraphQLInt},
+            },
+            resolve(parentValue, args){
+                return axios.patch(`http://localhost:3000/usuarios/${args.id}`, args)
+                .then(res => res.data)
+            }
+        },
+    }
+})
+
 // Root query (Resolve acessando os dados de testes locais)
  const rootQuery = new GraphQLObjectType({
     name: 'rootQuery',
     fields:{
         usuario : { 
-            type: UsuarioType,
+            type: usuarioType,
             args : {
                 id : {type: GraphQLInt}
             },
             resolve(parentValue, args){
-                for (let i = 0; i < usuarios.length; i++){
-                    if (usuarios[i].id == args.id){
-                        return usuarios[i]
-                    }
-                }
+               return axios.get(`http://localhost:3000/usuarios/${args.id}`)
+                        .then(res => res.data)
             }
         },
         usuarios :{
-            type: new GraphQLList(UsuarioType),
+            type: new GraphQLList(usuarioType),
             resolve(parentValue, args){
-                return usuarios
+                return axios.get(`http://localhost:3000/usuarios`)
+                        .then(res => res.data)
             }
         }
      }, 
  });
 
 module.exports = new GraphQLSchema({
-    query: rootQuery
+    query: rootQuery,
+    mutation
 });
